8dd87489d4faba7873d5c924d78e00a0d5c885bc
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2019-09-19 22:55:51 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,9 +2,8 @@ require 'pry'
 
 def my_find(collection)
   i = 0 
-  new_collection = []
   while i < collection.length 
-    new_collection << yield(collection[i])
+    return collection[i] if yield(collection[i])
     i += 1 
   end
 end
